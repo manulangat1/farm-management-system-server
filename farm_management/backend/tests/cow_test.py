ffae4ce1e12bd260930911cf8935247678f4b98e
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-17 16:57:51 +0300

    ft-adding_readme-backend

--- farm_management/backend/tests/cow_test.py
+++ farm_management/backend/tests/cow_test.py
@@ -7,21 +7,29 @@ import datetime
 def api_client():
     from rest_framework.test import APIClient
     return APIClient()
-# @pytest.mark.django_db
-# def test_cow_create(api_client):
-#     now = datetime.datetime.now()
-#     data = {
-#         'dob':now,
-#         'name':'manu',
-#         'stage':'HEIFER',
-#         'breed':'FRESIAN'
-#     }
-#     url = reverse('cow_create')
-#     response = api_client.post(url,data)
-#     assert response.status_code == 201
+@pytest.fixture(scope='session')
+def django_db_setup():
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'farm_management',
+        'USER':'manulangat',
+        'PASSWORD':'3050manu'
+    }
+@pytest.mark.django_db
+def test_cow_create(api_client,django_db_setup):
+    now = datetime.datetime.now()
+    data = {
+        'dob':now,
+        'name':'manu',
+        'stage':'HEIFER',
+        'breed':'FRESIAN'
+    }
+    url = reverse('cow_create')
+    response = api_client.post(url,data)
+    assert response.status_code == 201
 
 @pytest.mark.django_db
-def test_cow_list(api_client):
+def test_cow_list(api_client,django_db_setup):
     url = reverse('cow_list')
     response = api_client.get(url)
     assert response.status_code == 200

d26e3afaddbf665f60b2ef88a3fcb743d9b5ce02
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-17 16:01:12 +0300

    ft-testing_travis_ci_config-backend

--- farm_management/backend/tests/part_tests.py
+++ farm_management/backend/tests/part_tests.py
@@ -9,6 +9,5 @@ def api_client():
 @pytest.mark.django_db
 def test_part_list(api_client):
     url = reverse('parturation_list')
-    
     res = api_client.get(url)
-    assert res.status_code == 404
+    assert res.status_code == 200

1a5982c07eccb070d9ce3df998a924b6f1ea39fb
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-19 13:59:57 +0300

    ft_adding_swagger_ui-backend

--- farm_management/backend/urls.py
+++ farm_management/backend/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
 from .api import CowAPI,CowListAPI,CowDetails,ParturationAPI,ParturationListAPI,ParturationDetailAPI
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.schemas import get_schema_view
+schema_view = get_swagger_view(title='Farm Management Server API')
+
+
 urlpatterns = [
-    path('',CowAPI.as_view(),name="cow_create"),
+    path('cow_create/',CowAPI.as_view(),name="cow_create"),
     path('cows/',CowListAPI.as_view(),name="cow_list"),
     path('cows/<pk>',CowDetails.as_view(),name="cow_details"),
     path('part/',ParturationAPI.as_view(),name="parturation_create"),
     path('part/list/',ParturationListAPI.as_view(),name="parturation_list"),
 
     path('part/<pk>/',ParturationDetailAPI.as_view(),name="parturation_detail"),
+    path('openapi/', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('',schema_view)
 
 ]
--- farm_management/farm_management/settings.py
+++ farm_management/farm_management/settings.py
@@ -26,8 +26,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend.apps.BackendConfig',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_swagger',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
